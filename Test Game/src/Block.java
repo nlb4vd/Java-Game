0673d01a4df863cc2ee8373ff1003d52f1875ea3
nlb4vd <nlb4vd@virginia.edu>
2021-11-22 20:15:23 -0500

    Add files via upload

---
+++ Test Game/src/Block.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Color;
+
+public class Block extends GameObject {
+
+	public Block(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	@Override
+	public void tick() {
+
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(x, y, 32, 32);
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 32, 32);
+	}
+
+}
---
+++ Test Game/src/Handler.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.awt.Graphics;
+
+public class Handler {
+	
+	LinkedList<GameObject> object = new LinkedList<GameObject>();
+
+	private boolean up = false, down = false, right = false, left = false;
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public void tick() {
+		for (int i=0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for (int i=0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			tempObject.render(g);
+		}
+	}
+	
+	public void addObject(GameObject tempObject) {
+		object.add(tempObject);
+	}
+	
+	public void removeObject(GameObject tempObject) {
+		object.remove(tempObject);
+	}
+}
